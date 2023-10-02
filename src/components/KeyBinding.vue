b6939db25dd6e19b398c7573cfdc2bad622791d0
Bee <duy.ta@sotatek.com>
2023-10-03 04:19:10 +0700

    feat: add swipe for mobile

--- src/components/KeyBinding.vue
+++ src/components/KeyBinding.vue
@@ -1,15 +1,50 @@
 <script setup lang="ts">
+import { useSwipe } from '@/composables/useSwipe'
 import { Direction } from '@/models/direction'
 import useBlockStore from '@/stores/block'
-import { debounce } from 'lodash';
+import { debounce } from 'lodash'
 import { onMounted } from 'vue'
 
 const blockStore = useBlockStore()
 
+const { onSwipeLeft, onSwipeRight, onSwipeDown, onSwipeUp } = useSwipe(document.body)
+
+const preventEndGameAction = (callback: Function) => () => {
+  if (blockStore.isEndGame) {
+    return
+  }
+
+  callback()
+}
+
+onSwipeLeft(
+  preventEndGameAction(() => {
+    blockStore.move(Direction.LEFT)
+  })
+)
+
+onSwipeRight(
+  preventEndGameAction(() => {
+    blockStore.move(Direction.RIGHT)
+  })
+)
+
+onSwipeDown(
+  preventEndGameAction(() => {
+    blockStore.move(Direction.DOWN)
+  })
+)
+
+onSwipeUp(
+  preventEndGameAction(() => {
+    blockStore.move(Direction.UP)
+  })
+)
+
 const onKeyDown = (event: KeyboardEvent) => {
-  // if (blockStore.isEndGame) {
-  //   return
-  // }
+  if (blockStore.isEndGame) {
+    return
+  }
 
   switch (event.code) {
     case 'ArrowLeft':
